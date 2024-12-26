80ce9fb54214f961169be41dde4b81c29e7ec235
Md-Zakarya <130152604+Md-Zakarya@users.noreply.github.com>
2024-12-26 17:06:52 +0530

    Update index.js
    removed duplicate express.json

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ const io = setupSocketIO(httpServer);
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 
 // Routes
@@ -29,8 +31,7 @@ const messageRoutes = require('./routes/messageRoutes');
 const friendRoutes = require('./routes/friendRoutes');
 const userRoutes = require('./routes/userRoutes'); 
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
