07e74e955891c7db9aa35b8ceecd9e72e45e63f3
Md-Zakarya <130152604+Md-Zakarya@users.noreply.github.com>
2024-12-26 16:29:34 +0530

    Update index.js
    increased the maximum payload limit to 50mb

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,10 @@ const authRoutes = require('./routes/authRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const friendRoutes = require('./routes/friendRoutes');
 const userRoutes = require('./routes/userRoutes'); 
-app.use(express.json({ limit: '10mb' }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/friends', friendRoutes);
